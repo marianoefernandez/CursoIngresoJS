fcda7bdc191c48101d465750f102099330628203
marianoefernandez <47353464+marianoefernandez@users.noreply.github.com>
2019-02-06 23:54:16 -0300

    Resueltos los primeros tres puntos del parcial.

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,15 @@
 function mostrar()
 {
+	var precio;
+	var porcentaje;
+	var preciofinal;
 
+	precio = prompt ("Ingrese un precio inicial ($) ","");
+	precio = parseInt (precio);
+	porcentaje = prompt ("Ingrese su descuento (%) ","");
+	porcentaje = parseInt (porcentaje);
+
+	preciofinal = precio * porcentaje / 100;
+
+	elPrecioFinal.value = preciofinal; 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,17 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var perimetro;
+
+	ancho = prompt ("Ingrese el ancho (m)" , "");
+	ancho = parseInt (ancho);
+	largo = prompt ("Ingrese el largo (m)" , "");
+	largo = parseInt (largo);
+
+	perimetro = (ancho + largo) * 2;
+
+	alert ("El perimetro de este rectangulo es de: "+perimetro+ " metros");
 
 }
