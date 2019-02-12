27e90c1dcc731c8cbeae45e5e14c48e1086c6f3d
alumno <alumno>
2019-02-12 03:16:25 -0300

    Ejercicios 1 y 2

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,10 +1,24 @@
 function mostrar()
 {
-  	var nombre;
-	var localidad;
+  	var precio;
+	var recargo;
+	var tarjeta;
 
-  	nombre = elNombre.value;
-  	localidad = laLocalidad.value;
 
-  	alert ("usted es "+nombre+ " y vive en la localidad de " +localidad);
+  	//precio = ElPrecio.value;
+  	//precio = parseInt (precio); 
+  	precio = prompt ("Ingrese un precio ($) ","");
+	precio = parseInt (precio);
+
+  	recargo = (precio) * 10 / 100;
+
+  	tarjeta = precio + recargo;
+
+  	alert ("El precio de su producto en efectivo es de " +precio + " $ " + " Si lo paga con tarjeta se le cobra un 10% de recargo costandole " +tarjeta + " $" );
 }
+	//var precio;
+	//var porcentaje;
+	//var preciofinal;
+
+	//precio = prompt ("Ingrese un precio inicial ($) ","");
+	//precio = parseInt (precio);
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,17 +1,21 @@
 
 function mostrar()
 {
-	var ancho;
-	var largo;
-	var perimetro;
+	var persona1;
+	var edad1;
+	var persona2;
+	var edad2;
+	var sumaedades
 
-	ancho = prompt ("Ingrese el ancho (m)" , "");
-	ancho = parseInt (ancho);
-	largo = prompt ("Ingrese el largo (m)" , "");
-	largo = parseInt (largo);
+	persona1 = prompt ("Ingrese un nombre " , "");
+	edad1 = prompt ("Ingrese su edad " , "");
+	edad1 = parseInt (edad1);
 
-	perimetro = (ancho + largo) * 2;
+	persona2 = prompt ("Ingrese otro nombre " , "");
+	edad2 = prompt ("Ingrese otra edad " , "");
+	edad2 = parseInt (edad2);
 
-	alert ("El perimetro de este rectangulo es de: "+perimetro+ " metros");
+	sumaedades = (edad1 + edad2);
 
+	alert ("Ustedes son "+persona1+ " y " +persona2+ " y sus edades son " +edad1+ " y " +edad2+ " , y la suma de sus edades es " +sumaedades );
 }
