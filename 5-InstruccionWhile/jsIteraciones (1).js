9b9b53ca7c2fdcd3e4ebcf3f1c7d8adc8b70bb66
marianoefernandez <47353464+marianoefernandez@users.noreply.github.com>
2019-02-20 12:27:22 -0300

    Add files via upload
    CLASE 6

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,15 @@
 function mostrar()
 {
-	alert('iteración while');
+	var contador
+	contador= 0;
+
+	while (contador<10)
+	{
+		contador = contador + 1;
+		console.log (contador);
+	}
+			
 
 
 }//FIN DE LA FUNCIÓN
+//Toda interacion tiene un comienzo, un recorrido, y una acción que marca su final!.
--- 5-InstruccionWhile/jsIteraciones (2).js
+++ 5-InstruccionWhile/jsIteraciones (2).js
@@ -1,6 +1,13 @@
 function mostrar()
 {
-	alert('iteración while');
+	var contador
+	contador= 10;
+
+	while (contador<0)
+	{
+		contador = contador - 1;
+		console.log (contador);
+	}
 
 
 }//FIN DE LA FUNCIÓN
