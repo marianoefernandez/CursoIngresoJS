ee5af021cd22d67489ba75a413be68c2cd359d9c
marianoefernandez <47353464+marianoefernandez@users.noreply.github.com>
2019-02-20 12:25:54 -0300

    Add files via upload
    CLASE 4

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,64 @@
 function mostrar()
 {
+	var habitación;
+	var mediodepago; 
+	var preciototal; 
+	var descuento;
+	var total;
+	var paquetes
+
+
+	habitación = prompt ("Elija su habitación: (1- Cuesta 100$ y no tiene tv 2- Cuesta 300$ y tiene tv 3- cuesta 500$ y tiene 2 tv, equipo de musica, pc y lavarropas"  , "");
+	habitación = parseInt (habitación);
+	mediodepago = prompt ("Elija el tipo de pago (1-Efectivo 2-Visa 3-Paypal 4-MercadoPago 5-Otro", ""); 
+	switch (mediodepago)
+	{
+		case 1:
+			descuento = 20
+			paquetes = prompt ("Ha pagado en efectivo puede seleccionar los siguientes paquetes con un descuento 1-Solodesayunos (100$ +10% al descuento total), 2-todoincluido (200$ +15% de descuento), 3-Paquete de bebida, (100$) 4- No quiero paquete", "");
+			break;
+		case 3:
+			descuento = 15
+			paquetes = prompt ("Ha pagado con Paypal Seleccione los siguientes paquetes 1-Solodesayunos (100$), 2-todoincluido (200$  +10% de descuento total), 3-Paquete de bebida (100$) 4-No quiero paquete", "");
+			break;	
+		case 2:
+		case 4:
+			descuento = 10
+			paquetes = prompt ("Seleccione los siguientes paquetes 1-Solodesayunos (100$), 2-todoincluido (200$), 3-Paquete de bebida (100$) 4-No quiero paquete", "");
+			break;	
+		case 5:	
+			descuento = 5
+			paquetes = prompt ("Seleccione los siguientes paquetes 1-Solodesayunos (100$), 2-todoincluido (200$), 3-Paquete de bebida (100$) 4-No quiero paquete", "");
+			break;	
+	}		
+
+	switch (habitación)
+		{
+			case 1:
+			habitación = 100
+			case 2:
+			habitación = 300
+			case 3:
+			habitación = 500
+		}
+	switch (mediodepago)
+		{
+			case 1:
+			descuento = 20
+			case 2:
+			case 4:
+			descuento = 10
+			case 5: 
+			descuento = 5
+			case 3
+			descuento = 15
+		}
+	
 
 }
+/* USAR SOLO SWITCH
+cada habitación de un hotel tiene un precio, hay promociones según el tipo de pago, si es con tarjeta visa un 10%,
+efectivo 20% otro medio 5%.
+Si compraste el paquete "todoincluido" y pagas con Paypal se te suma un 10% al descuento 
+si pagas en efectivo tenes varias opciones, el paquete "soloDesayunos" te suma un 10% al descuento, si el paquete 
+es "todoincluido" te suma un 15% y para el resto de los paquetes no tiene descuento adicional.
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,65 @@
 function mostrar()
 {
+	var dato1;
+	var dato2;
+	var dato3;
+	var dato4;
+	var mayor;
+	var total;
 
+	dato1 =  prompt ("ingrese un importe", "");
+	dato1 = parseInt (dato1);
+	dato2 =  prompt ("ingrese otro importe", "");
+	dato2 = parseInt (dato2);
+	dato3 =  prompt ("ingrese otro importe", "");
+	dato3 = parseInt (dato3);
+	dato4 =  prompt ("ingrese otro importe", "");
+	dato4 = parseInt (dato4);
+
+	total = dato1 + dato2 + dato3 + dato4;
+
+	if (dato1 > dato2 && dato1 > dato3 && dato1 > dato4)
+		{
+			mayor = dato1;
+		}
+		else
+		{
+			if (dato2 > dato3 && dato2 > dato4)
+			{
+				mayor = dato2;
+			}
+			else
+			{
+				if (dato3 > dato4)
+				{
+					mayor = dato3;
+				}
+				else 
+				{
+					mayor = dato4;
+				}
+			}
+		}
+	if (total > 100)
+	{
+		total = total * 90 / 100;
+	}
+	else
+	{	
+		if (total > 50)
+		{
+			total = total * 95 / 100;
+		}
+		else
+			{
+				total = total + (total / 100 * 15);
+			}
+
+	}
+
+	alert ("El mayor de la compra es: " + mayor + " y el total es: " + total);
 }
+//Un sistema que ingrese 4 importes, mostrar el mayor de la compra,
+//después decir el total, si supera 100 pesos descuento del 10%
+//si supera los 50$ es el 5%
+//si es menos de 50$ se le suma el 15%
